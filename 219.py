a4a3e1dec7fc0f33ebda5ef930b63e7e940dfd35
tanbiralam <itzmetanbir@gmail.com>
2025-05-12 22:20:58 +0530

    219

---
+++ 219.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/contains-duplicate-ii/
+
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        hset = {}
+        for idx in range (len(nums)):
+            if nums[idx] in hset and abs(idx - hset[nums[idx]]) <= k:
+                return True
+            hset[nums[idx]] = idx
+        return False
